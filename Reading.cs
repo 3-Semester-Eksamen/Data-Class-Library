2092f1c43b072a8231c2a2f913a3ba4af9811489
Mads <Mads@DESKTOP-R1NEBSS>
2022-12-05 10:41:05 +0100

    Add project files.

---
+++ Reading.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Class_Library
+{
+    public class Reading
+    {
+        public int Id { get; set; }
+        public string MacAddressSensor { get; set; }
+        public int OpenedBy { get; set; }
+        public string Time = DateTime.Now.ToString();
+
+        public void ValidateMacAddressSensor()
+        {
+
+            if (string.IsNullOrWhiteSpace(MacAddressSensor)) throw new ArgumentNullException("Navnet må ikke være tomt");
+            if (MacAddressSensor.Length < 3) throw new ArgumentOutOfRangeException("Navnet skal være længere end 1 karakter");
+        }
+
+        public void ValidateOpenedBy()
+        {
+            if (OpenedBy == null) throw new ArgumentNullException("Feltet må ikke være tomt");
+            if (OpenedBy < 0) throw new ArgumentOutOfRangeException("Feltet må ikke være negativt");
+        }
+
+
+        public void Validate()
+        {
+            ValidateMacAddressSensor();
+            ValidateOpenedBy();
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(MacAddressSensor)}={MacAddressSensor}, {nameof(OpenedBy)}={OpenedBy.ToString()}, {nameof(Time)}={Time.ToString()}}}";
+        }
+    }
+}
---
+++ Sensor.cs
@@ -0,0 +1,31 @@
+﻿namespace Class_Library
+{
+    public class Sensor
+    {
+        public string MacAddress { get; set; }
+        public string Name { get; set; }
+
+        public void ValidateMacAddress()
+        {
+            if (string.IsNullOrWhiteSpace(MacAddress)) throw new ArgumentNullException("MacAddresse må ikke være tom");
+            if (MacAddress.Length <= 2) throw new ArgumentOutOfRangeException("MacAddresse skal være længere end 2 karakterer lang" + MacAddress);
+        }
+
+        public void ValidateName()
+        {
+            if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentNullException("Navnet må ikke være tomt");
+            if (Name.Length <= 2) throw new ArgumentOutOfRangeException("Navnet skal være længere end 2 karakterer langt");
+        }
+
+        public void Validate()
+        {
+            ValidateMacAddress();
+            ValidateName();
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(MacAddress)}={MacAddress}, {nameof(Name)}={Name}}}";
+        }
+    }
+}
